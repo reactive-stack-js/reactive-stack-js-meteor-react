03e73d7576223877b3fec82ed7ea55c77df4b112
Predrag Stojadinovic <predrag@stojadinovic.net>
2020-10-10 16:52:48 +0200

    codacy

--- client/main.js
+++ client/main.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import {Meteor} from 'meteor/meteor';
-import {render} from 'react-dom';
-import {renderApp} from '../imports/ui/App.js';
+import React from "react";
+import {Meteor} from "meteor/meteor";
+import {render} from "react-dom";
+import {renderApp} from "../imports/ui/App.js";
 
-import '../imports/startup/accounts-config.js';
+import "../imports/startup/accounts-config.js";
 
 Meteor.startup(() => render(
 	renderApp(),
-	document.getElementById('reactive-stack-app')
+	document.getElementById("reactive-stack-app")
 ));
--- server/main.js
+++ server/main.js
@@ -1,4 +1,4 @@
-import '../imports/api/lorems.js';
-import '../imports/api/drafts.js';
+import "../imports/api/lorems.js";
+import "../imports/api/drafts.js";
 
-console.log('meteor-react started...');
+console.log("meteor-react started...");
